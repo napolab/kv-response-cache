ae599ddf5d518f7bf05d9ede44ee9432a75db362
naporitan <32933709+naporin0624@users.noreply.github.com>
2023-06-20 12:13:34 +0900

    Re export (#9)
    * :+1: create middleware file
    
    * :+1: entry file
    
    * :+1: add doc
    
    * 🎨 format code

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,8 @@
-import { kvResponseCache } from "./caches"
+import { kvResponseCache } from "./caches";
+import { kvCaches } from "./middleware";
 
-import type { Context, Env, MiddlewareHandler } from "hono"
-import type { Filter } from "./types"
+import type { KVResponseCache } from "./caches";
 
-
-type Namespace<E extends Env> = string | ((c: Context<E>) => string)
-type KVCacheOption<E extends Env> = {
-  key: keyof Filter<Exclude<E["Bindings"], undefined>, KVNamespace>
-  namespace: Namespace<E>
-  options?: KVNamespacePutOptions
-}
-
-export const kvCaches = <E extends Env>({ namespace, key: bindingKey, options }: KVCacheOption<E>): MiddlewareHandler<E> => async (c, next) => {
-  const kv: KVNamespace = c.env?.[bindingKey as string] as KVNamespace
-  const kvNamespace = typeof namespace === "function" ? namespace(c) : namespace
-
-  const kvCaches = kvResponseCache(kv)
-  const cache = kvCaches(kvNamespace)
-
-  const key = new URL(c.req.url).pathname;
-  const response = await cache.match(key);
-  if (response) return response;
-
-  await next()
-
-  c.executionCtx.waitUntil(cache.put(key, c.res.clone(), options));
-}
+export type { KVResponseCache };
+export { kvResponseCache };
+export { kvCaches };
