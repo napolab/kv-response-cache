96674d84f73bb62d32310a14d00d122a111fd255
naporin0624 <naporin0624@gmail.com>
2023-09-20 20:20:18 +0900

    :bug: Changed cache middleware to cache only when status code is 2xx

--- src/middleware.test.ts
+++ src/middleware.test.ts
@@ -56,4 +56,24 @@ describe("kvCaches", () => {
     const cachedRes = await env.KV.get("test:" + new URL(req.url).pathname + ":body", "text");
     expect(cachedRes).toEqual("Hello, World!");
   });
+
+  it("does not cache the response if status code is not 2xx", async () => {
+    app.get("/error", kvCache, async (c) => {
+      return c.body("Error", { status: 500 });
+    });
+
+    const req1 = new Request("http://localhost/error");
+    const res1 = await app.fetch(req1, env, ctx);
+    expect(res1.status).toEqual(500);
+    expect(res1.headers.get("X-KV-CACHE")).toBeNull(); // First request should not be cached
+    await getMiniflareWaitUntil(ctx);
+
+    const req2 = new Request("http://localhost/error");
+    const res2 = await app.fetch(req2, env, ctx);
+    expect(res2.status).toEqual(500);
+    expect(res2.headers.get("X-KV-CACHE")).toBeNull(); // Second request should not be cached as well
+
+    const cachedRes = await env.KV.get("test:" + new URL(req1.url).pathname + ":body", "text");
+    expect(cachedRes).toBeNull(); // Response should not be cached
+  });
 });
